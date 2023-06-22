2d844a84edd00cfb53709e6f0df0f75235c0a9ee
Gijs Burghoorn <g.burghoorn@gmail.com>
2023-06-22 19:48:45 +0200

    Refactor: Split out WaveJson into separate categories

---
+++ wavedrom/src/wavejson/mod.rs
@@ -0,0 +1,48 @@
+//! The definitions for the WaveJson format.
+#![allow(missing_docs)]
+
+use serde::{Deserialize, Serialize};
+
+use self::signal::SignalJson;
+
+use crate::Figure;
+
+pub mod signal;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum WaveJson {
+    Signal(SignalJson),
+}
+
+impl Figure {
+    #[cfg(feature = "serde_json")]
+    pub fn from_json(s: &str) -> Result<Self, serde_json::Error> {
+        WaveJson::from_json(s).map(Into::into)
+    }
+
+    #[cfg(feature = "json5")]
+    pub fn from_json5(s: &str) -> Result<Self, json5::Error> {
+        WaveJson::from_json5(s).map(Into::into)
+    }
+}
+
+impl WaveJson {
+    #[cfg(feature = "serde_json")]
+    pub fn from_json(s: &str) -> Result<Self, serde_json::Error> {
+        serde_json::from_str(s)
+    }
+
+    #[cfg(feature = "json5")]
+    pub fn from_json5(s: &str) -> Result<Self, json5::Error> {
+        json5::from_str(s)
+    }
+}
+
+impl From<WaveJson> for Figure {
+    fn from(value: WaveJson) -> Self {
+        match value {
+            WaveJson::Signal(signal_json) => signal_json.into(),
+        }
+    }
+}
---
+++ wavedrom/src/wavejson/signal.rs
@@ -0,0 +1,179 @@
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    CycleEnumerationMarker, CycleOffset, CycleState, Figure, FigureSection,
+    FigureSectionGroup, Signal,
+};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SignalJson {
+    pub signal: Vec<SignalItem>,
+    pub head: Option<Head>,
+    pub foot: Option<Foot>,
+    pub config: Option<Config>,
+    pub edge: Option<Vec<String>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum SignalItem {
+    Group(Vec<SignalGroupItem>),
+    Item(SignalObject),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum SignalGroupItem {
+    String(String),
+    Item(SignalItem),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SignalObject {
+    pub name: Option<String>,
+    pub wave: Option<String>,
+    pub data: Option<SignalData>,
+    pub node: Option<String>,
+    pub period: Option<f32>,
+    pub phase: Option<f32>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum SignalData {
+    One(String),
+    Multiple(Vec<String>),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Head {
+    pub text: Option<String>,
+    pub tick: Option<u32>,
+    pub every: Option<u32>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Foot {
+    pub text: Option<String>,
+    pub tock: Option<u32>,
+    pub every: Option<u32>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Config {
+    pub hscale: Option<u16>,
+    pub skin: Option<String>,
+}
+
+impl From<SignalJson> for Figure {
+    fn from(value: SignalJson) -> Self {
+        let (title, top_cycle_marker) = if let Some(head) = value.head {
+            let title = head.text;
+            let top_cycle_marker = match (head.tick, head.every) {
+                (Some(start), Some(every)) => Some(CycleEnumerationMarker::new(start, every)),
+                (Some(start), None) => Some(CycleEnumerationMarker::new(start, 1)),
+                (None, _) => None,
+            };
+
+            (title, top_cycle_marker)
+        } else {
+            (None, None)
+        };
+        let (footer, bottom_cycle_marker) = if let Some(foot) = value.foot {
+            let footer = foot.text;
+            let bottom_cycle_marker = match (foot.tock, foot.every) {
+                (Some(start), Some(every)) => Some(CycleEnumerationMarker::new(start, every)),
+                (Some(start), None) => Some(CycleEnumerationMarker::new(start, 1)),
+                (None, _) => None,
+            };
+
+            (footer, bottom_cycle_marker)
+        } else {
+            (None, None)
+        };
+
+        let hscale = value.config.and_then(|config| config.hscale).unwrap_or(1);
+
+        let sections = value
+            .signal
+            .into_iter()
+            .map(FigureSection::from)
+            .collect::<Vec<FigureSection>>();
+
+        let mut edges = Vec::new();
+
+        if let Some(edge) = value.edge {
+            for e in edge {
+                if let Ok(def) = e.parse() {
+                    edges.push(def);
+                }
+            }
+        }
+
+        Figure::with(
+            title,
+            footer,
+            top_cycle_marker,
+            bottom_cycle_marker,
+            hscale,
+            sections,
+            edges,
+        )
+    }
+}
+
+impl From<SignalItem> for FigureSection {
+    fn from(signal: SignalItem) -> Self {
+        match signal {
+            SignalItem::Group(items) => {
+                let mut label = None;
+
+                let items = items
+                    .into_iter()
+                    .filter_map(|item| match item {
+                        SignalGroupItem::String(s) => {
+                            if label.is_none() {
+                                label = Some(s);
+                            }
+
+                            None
+                        }
+                        SignalGroupItem::Item(line) => Some(FigureSection::from(line)),
+                    })
+                    .collect::<Vec<FigureSection>>();
+
+                FigureSection::Group(FigureSectionGroup(label, items))
+            }
+            SignalItem::Item(item) => FigureSection::Signal(Signal::from(item)),
+        }
+    }
+}
+
+impl From<SignalObject> for Signal {
+    fn from(item: SignalObject) -> Self {
+        let name = item.name.unwrap_or_default();
+        let cycles = item
+            .wave
+            .unwrap_or_default()
+            .chars()
+            .map(CycleState::from)
+            .collect();
+        let data = item
+            .data
+            .map_or_else(Vec::new, |signal_data| match signal_data {
+                SignalData::One(data) => data
+                    .split(char::is_whitespace)
+                    .filter(|s| !s.is_empty())
+                    .map(str::to_string)
+                    .collect(),
+                SignalData::Multiple(data) => data,
+            });
+        let node = item.node.unwrap_or_default();
+        let period = item.period.map_or(0, |f| f.ceil() as u16);
+        let phase = item.phase.map_or_else(CycleOffset::default, |f| {
+            CycleOffset::try_from(f).unwrap_or_default()
+        });
+
+        Signal::with(name, cycles, data, node, period, phase)
+    }
+}
