8780829b7d099fbb788852f611160fb79923eaa0
Gijs Burghoorn <g.burghoorn@gmail.com>
2023-06-28 22:36:46 +0200

    Fix: Inprecision because of rect dimension calculations

--- wavedrom/src/reg/render/mod.rs
+++ wavedrom/src/reg/render/mod.rs
@@ -4,30 +4,36 @@ use crate::Font;
 
 use super::Register;
 
+fn to_display_num(n: f64) -> f64 {
+    (n * 1000.).round() / 1000.
+}
+
 impl Register {
     pub fn write_svg(&self, writer: &mut impl io::Write) -> io::Result<()> {
-        const WIDTH: u32 = 400;
-        const HEIGHT: u32 = 100;
+        const WIDTH: f64 = 400.;
+        const HEIGHT: f64 = 100.;
 
-        const PAD_LEFT: u32 = 5;
-        const PAD_RIGHT: u32 = 5;
+        const PAD_LEFT: f64 = 5.;
+        const PAD_RIGHT: f64 = 5.;
 
-        const PAD_TOP: u32 = 25;
+        const PAD_TOP: f64 = 25.;
 
-        const BAR_WIDTH: u32 = WIDTH - PAD_LEFT - PAD_RIGHT;
-        const BAR_HEIGHT: u32 = 20;
+        const BAR_WIDTH: f64 = WIDTH - PAD_LEFT - PAD_RIGHT;
+        const BAR_HEIGHT: f64 = 20.;
 
-        const NAME_FONTSIZE: u32 = BAR_HEIGHT * 2 / 5;
-        const BITMARKER_FONTSIZE: u32 = 6;
-        const ATTRIBUTE_FONTSIZE: u32 = NAME_FONTSIZE;
+        const BAR_MIDDLE: f64 = (PAD_TOP + PAD_TOP + BAR_HEIGHT) / 2.;
 
-        const HINT_INDENT: u32 = BAR_HEIGHT / 10;
+        const NAME_FONTSIZE: f64 = BAR_HEIGHT * 2. / 5.;
+        const BITMARKER_FONTSIZE: f64 = 6.;
+        const ATTRIBUTE_FONTSIZE: f64 = NAME_FONTSIZE;
 
-        const SE_MARKER_X_OFFSET: u32 = 2;
-        const SE_MARKER_Y_OFFSET: u32 = 2;
+        const HINT_INDENT: f64 = BAR_HEIGHT / 10.;
 
-        const ATTRIBUTE_Y_OFFSET: u32 = 2;
-        const ATTRIBUTE_Y_SPACING: u32 = 4;
+        const SE_MARKER_X_OFFSET: f64 = 2.;
+        const SE_MARKER_Y_OFFSET: f64 = 2.;
+
+        const ATTRIBUTE_Y_OFFSET: f64 = 2.;
+        const ATTRIBUTE_Y_SPACING: f64 = 4.;
 
         let font = Font::default();
 
@@ -47,10 +53,12 @@ impl Register {
             return Ok(());
         }
 
+        let bit_width = f64::from(self.width);
+
         write!(
             writer,
             r##"<defs><g id="bm"><path d="M0,0v{HINT_INDENT}m0,{jump}v{HINT_INDENT}" stroke="#000" fill="none"/></g></defs>"##,
-            jump = BAR_HEIGHT - 2 * HINT_INDENT,
+            jump = BAR_HEIGHT - 2. * HINT_INDENT,
         )?;
 
         write!(writer, "<g>")?;
@@ -72,11 +80,18 @@ impl Register {
                     _ => "#5B5",
                 };
 
+                let width = to_display_num(if offset_end == self.width {
+                    BAR_WIDTH - f64::from(offset_start) * BAR_WIDTH / bit_width
+                } else {
+                    f64::from(bit_range.length) * BAR_WIDTH / bit_width
+                });
+
                 write!(
                     writer,
                     r##"<path d="M{x},{PAD_TOP}h{width}v{BAR_HEIGHT}h-{width}v-{BAR_HEIGHT}z" stroke="none" fill="{background}"/>"##,
-                    x = PAD_LEFT + offset_start * BAR_WIDTH / self.width,
-                    width = bit_range.length * BAR_WIDTH / self.width,
+                    x = to_display_num(
+                        PAD_LEFT + BAR_WIDTH - f64::from(offset_end) * BAR_WIDTH / bit_width
+                    ),
                 )?;
             }
 
@@ -92,58 +107,62 @@ impl Register {
             let offset_start = offset;
             let offset_end = offset + bit_range.length;
 
-            let offset_center = (offset_start + offset_end) / 2;
-
             // Draw bit hint markers
-            for i in offset + 1..offset + bit_range.length {
+            for i in offset_start + 1..offset_end {
+                let i = f64::from(i);
+
                 write!(
                     writer,
                     r##"<use transform="translate({x},{y})" xlink:href="#bm"/>"##,
-                    x = PAD_LEFT + i * BAR_WIDTH / self.width,
+                    x = to_display_num(PAD_LEFT + BAR_WIDTH - i * BAR_WIDTH / bit_width),
                     y = PAD_TOP
                 )?;
             }
 
+            let offset_center = f64::from(offset_start + offset_end) / 2.;
+
             // Draw the field name
             if let Some(name) = &bit_range.name {
-                let needs_half = (bit_range.length & 1) == 1;
-
-                // TODO: Better Centering
                 write!(
                     writer,
                     r##"<text x="{x}" y="{y}" text-anchor="middle" dominant-baseline="middle" font-family="{font_family}" font-size="{NAME_FONTSIZE}" fill="#000" letter-spacing="0"><tspan>{name}</tspan></text>"##,
-                    x = PAD_LEFT
-                        + (offset_center * BAR_WIDTH + if needs_half { BAR_WIDTH / 2 } else { 0 })
-                            / self.width,
-                    y = (PAD_TOP + PAD_TOP + BAR_HEIGHT) / 2,
+                    x = to_display_num(
+                        PAD_LEFT + BAR_WIDTH - offset_center * BAR_WIDTH / bit_width
+                    ),
+                    y = BAR_MIDDLE,
                 )?;
             }
 
             // Draw the start and end markers
             if bit_range.length == 1 {
-                let needs_half = (bit_range.length & 1) == 1;
-
                 // TODO: Better Centering
                 write!(
                     writer,
                     r##"<text x="{x}" y="{y}" text-anchor="middle" dominant-baseline="text-bottom" font-family="{font_family}" font-size="{BITMARKER_FONTSIZE}" fill="#000" letter-spacing="0"><tspan>{offset_start}</tspan></text>"##,
-                    x = PAD_LEFT
-                        + (offset_center * BAR_WIDTH + if needs_half { BAR_WIDTH / 2 } else { 0 })
-                            / self.width,
+                    x = to_display_num(
+                        PAD_LEFT + BAR_WIDTH - (offset_center * BAR_WIDTH) / bit_width
+                    ),
                     y = PAD_TOP - SE_MARKER_Y_OFFSET,
                 )?;
             } else {
                 write!(
                     writer,
-                    r##"<text x="{x}" y="{y}" text-anchor="start" dominant-baseline="text-bottom" font-family="{font_family}" font-size="{BITMARKER_FONTSIZE}" fill="#000" letter-spacing="0"><tspan>{text}</tspan></text>"##,
-                    x = PAD_LEFT + offset_start * BAR_WIDTH / self.width + SE_MARKER_X_OFFSET,
+                    r##"<text x="{x}" y="{y}" text-anchor="end" dominant-baseline="text-bottom" font-family="{font_family}" font-size="{BITMARKER_FONTSIZE}" fill="#000" letter-spacing="0"><tspan>{text}</tspan></text>"##,
+                    x = to_display_num(
+                        PAD_LEFT + BAR_WIDTH
+                            - f64::from(offset_start) * BAR_WIDTH / bit_width
+                            - SE_MARKER_X_OFFSET
+                    ),
                     y = PAD_TOP - SE_MARKER_Y_OFFSET,
                     text = offset_start,
                 )?;
                 write!(
                     writer,
-                    r##"<text x="{x}" y="{y}" text-anchor="end" dominant-baseline="text-bottom" font-family="{font_family}" font-size="{BITMARKER_FONTSIZE}" fill="#000" letter-spacing="0"><tspan>{text}</tspan></text>"##,
-                    x = PAD_LEFT + offset_end * BAR_WIDTH / self.width - SE_MARKER_X_OFFSET,
+                    r##"<text x="{x}" y="{y}" text-anchor="start" dominant-baseline="text-bottom" font-family="{font_family}" font-size="{BITMARKER_FONTSIZE}" fill="#000" letter-spacing="0"><tspan>{text}</tspan></text>"##,
+                    x = to_display_num(
+                        PAD_LEFT + BAR_WIDTH - f64::from(offset_end) * BAR_WIDTH / bit_width
+                            + SE_MARKER_X_OFFSET
+                    ),
                     y = PAD_TOP - SE_MARKER_Y_OFFSET,
                     text = offset_end - 1,
                 )?;
@@ -156,18 +175,18 @@ impl Register {
 
                 let i = i as u32;
 
-                let needs_half = (bit_range.length & 1) == 1;
-
                 write!(
                     writer,
                     r##"<text x="{x}" y="{y}" text-anchor="middle" dominant-baseline="hanging" font-family="{font_family}" font-size="{ATTRIBUTE_FONTSIZE}" fill="#000" letter-spacing="0"><tspan>{offset_start}</tspan></text>"##,
-                    x = PAD_LEFT
-                        + (offset_center * BAR_WIDTH + if needs_half { BAR_WIDTH / 2 } else { 0 })
-                            / self.width,
-                    y = PAD_TOP
-                        + BAR_HEIGHT
-                        + ATTRIBUTE_Y_OFFSET
-                        + (ATTRIBUTE_FONTSIZE + ATTRIBUTE_Y_SPACING) * i,
+                    x = to_display_num(
+                        PAD_LEFT + BAR_WIDTH - (offset_center * BAR_WIDTH) / bit_width
+                    ),
+                    y = to_display_num(
+                        PAD_TOP
+                            + BAR_HEIGHT
+                            + ATTRIBUTE_Y_OFFSET
+                            + (ATTRIBUTE_FONTSIZE + ATTRIBUTE_Y_SPACING) * f64::from(i)
+                    ),
                 )?;
             }
 
@@ -181,7 +200,9 @@ impl Register {
             write!(
                 writer,
                 r##"<line x1="{x}" y1="{PAD_TOP}" x2="{x}" y2="{bar_bottom}" stroke="#000"/>"##,
-                x = PAD_LEFT + (offset * BAR_WIDTH) / self.width,
+                x = to_display_num(
+                    PAD_LEFT + BAR_WIDTH - (f64::from(offset) * BAR_WIDTH) / bit_width
+                ),
                 bar_bottom = PAD_TOP + BAR_HEIGHT,
             )?;
         }
